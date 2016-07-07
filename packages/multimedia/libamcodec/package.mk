7b42aa8e75cb2231a2d91da6ffa8156b8e2d17af
alex <surkov.alex@gmail.com>
2016-07-07 17:32:19 +0300

    libamcodec: allow building for aarch64

--- packages/multimedia/libamcodec/package.mk
+++ packages/multimedia/libamcodec/package.mk
@@ -19,7 +19,7 @@
 PKG_NAME="libamcodec"
 PKG_VERSION="45a1086"
 PKG_REV="1"
-PKG_ARCH="arm"
+PKG_ARCH="arm aarch64"
 PKG_LICENSE="other"
 PKG_SITE="http://openlinux.amlogic.com"
 PKG_URL="https://github.com/codesnake/libamcodec/archive/$PKG_VERSION.tar.gz"
