8fa359c005eee0622d6641b5b2d6fa452461a326
alex <surkov.alex@gmail.com>
2016-12-19 20:30:11 +0300

    packages/multimedia/libamcodec: Enable package for aarch64 architecture

--- packages/multimedia/libamcodec/package.mk
+++ packages/multimedia/libamcodec/package.mk
@@ -19,7 +19,7 @@
 PKG_NAME="libamcodec"
 PKG_VERSION="2fba80c"
 PKG_REV="1"
-PKG_ARCH="arm"
+PKG_ARCH="arm aarch64"
 PKG_LICENSE="other"
 PKG_SITE="http://openlinux.amlogic.com"
 PKG_URL="https://github.com/surkovalex/libamcodec/archive/$PKG_VERSION.tar.gz"
