f265e2daac39fe7503223ec9d3bc04607782042d
alex <surkov.alex@gmail.com>
2017-05-26 11:45:02 +0300

    separate platform_init for arm and aarch64

--- packages/sysutils/busybox/package.mk
+++ packages/sysutils/busybox/package.mk
@@ -239,10 +239,10 @@ makeinstall_init() {
     touch $INSTALL/etc/fstab
     ln -sf /proc/self/mounts $INSTALL/etc/mtab
 
-  if [ -n "$DEVICE" -a -f $PROJECT_DIR/$PROJECT/devices/$DEVICE/initramfs/platform_init ]; then
-    cp $PROJECT_DIR/$PROJECT/devices/$DEVICE/initramfs/platform_init $INSTALL
-  elif [ -f $PROJECT_DIR/$PROJECT/initramfs/platform_init ]; then
-    cp $PROJECT_DIR/$PROJECT/initramfs/platform_init $INSTALL
+  if [ -n "$DEVICE" -a -f $PROJECT_DIR/$PROJECT/devices/$DEVICE/initramfs/platform_init.$TARGET_ARCH ]; then
+    cp $PROJECT_DIR/$PROJECT/devices/$DEVICE/initramfs/platform_init.$TARGET_ARCH $INSTALL/platform_init
+  elif [ -f $PROJECT_DIR/$PROJECT/initramfs/platform_init.$TARGET_ARCH ]; then
+    cp $PROJECT_DIR/$PROJECT/initramfs/platform_init.$TARGET_ARCH $INSTALL/platform_init
   fi
   if [ -f $INSTALL/platform_init ]; then
     chmod 755 $INSTALL/platform_init
